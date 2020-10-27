b45201322d893dd1e3049d11952237b439de45ec
LIYICHENG1997 <zm19124@josai.ac.jp>
2020-10-27 02:24:32 +0000

    1027

---
+++ ex06-7.js
@@ -0,0 +1,8 @@
+var who67 = 'world';
+function sayhello67() {alert('Hello, ' + who67 + '!');}
+function someone(x) {who67 = x;}
+function someone(太郎) {who67 = 太郎;}
+function someone(花子) {who67 = 花子;}
+function someone(レイ) {who67 = レイ;}
+function someone(シンジ) {who67 = シンジ;}
+function someone(アスカ) {who67 = アスカ;}
